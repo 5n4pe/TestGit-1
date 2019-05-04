fd122449f91364781c65c7fae8270f7003540c3d
5n4pe_glb <snapewang_glb@outlook.com>
2019-05-04 16:45:22 +0800

    Test Conflict Right

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,8 +4,6 @@ public class Happy {
 	
 	public static void main(String[] args) {
 		System.out.println("right ...");
-		System.out.println("left ...");
-		System.out.println("hot fix ...");
 	}
 
 }
